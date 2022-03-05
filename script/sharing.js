81627bab7e0792103f5cd2cbe1d0b9b6df2c4a1f
Jongwook Park <nedpark@outlook.com>
2022-03-05 17:00:49 +0900

    added: sharing logs

--- script/sharing.js
+++ script/sharing.js
@@ -13,16 +13,31 @@ const shareKakaoTalk = () => {
             webUrl: window.location.href,
         },
     })
+
+    gtag('event', '공유선택: 카오톡', {
+        'event_category': '공유',
+        'event_lavel': `카카오톡`
+    })
 }
 
 const shareTelegram = () => {
     let url = encodeURIComponent(window.location.href)
     let title = encodeURIComponent("공약한판정리 - 기호1번 이재명")
-    window.open("https://telegram.me/share/url?url=" + url + "&text=" + title, "_blank");
+    window.open("https://telegram.me/share/url?url=" + url + "&text=" + title, "_blank")
+
+    gtag('event', '공유선택: 텔레그램', {
+        'event_category': '공유',
+        'event_lavel': `텔레그램`
+    })
 }
 
 const shareLink = () => {
     navigator.clipboard.writeText(window.location.href);
     alert("주소가 복사되었습니다 : " + window.location.href);
+
+    gtag('event', '공유선택: 주소복사', {
+        'event_category': '공유',
+        'event_lavel': `주소복사`
+    })
 }
 
