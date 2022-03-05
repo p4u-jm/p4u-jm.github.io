24c3ef3b0be95424ba7dba3fa2f26b070c8fe01f
Jongwook Park <nedpark@outlook.com>
2022-03-05 17:12:00 +0900

    Update sharing.js

--- script/sharing.js
+++ script/sharing.js
@@ -1,5 +1,15 @@
 const shareFacebook = () => {
     window.open("https://www.facebook.com/sharer/sharer.php?u=" + encodeURIComponent(window.location.href), "_blank");
+
+    gtag('event', '공유선택', {
+        'event_category': '공유',
+        'event_lavel': `페이스북`
+    })
+
+    gtag('event', '공유선택: 페이스북', {
+        'event_category': '공유',
+        'event_lavel': `페이스북`
+    })
 }
 
 Kakao.init('c462c25f06813a2fa3bafcb3f80c77e1');
@@ -14,7 +24,12 @@ const shareKakaoTalk = () => {
         },
     })
 
-    gtag('event', '공유선택: 카오톡', {
+    gtag('event', '공유선택', {
+        'event_category': '공유',
+        'event_lavel': `카카오톡`
+    })
+
+    gtag('event', '공유선택: 카카오톡', {
         'event_category': '공유',
         'event_lavel': `카카오톡`
     })
@@ -25,6 +40,11 @@ const shareTelegram = () => {
     let title = encodeURIComponent("공약한판정리 - 기호1번 이재명")
     window.open("https://telegram.me/share/url?url=" + url + "&text=" + title, "_blank")
 
+    gtag('event', '공유선택', {
+        'event_category': '공유',
+        'event_lavel': `텔레그램`
+    })
+
     gtag('event', '공유선택: 텔레그램', {
         'event_category': '공유',
         'event_lavel': `텔레그램`
@@ -34,6 +54,11 @@ const shareTelegram = () => {
 const shareLink = () => {
     navigator.clipboard.writeText(window.location.href);
     alert("주소가 복사되었습니다 : " + window.location.href);
+
+    gtag('event', '공유선택', {
+        'event_category': '공유',
+        'event_lavel': `주소복사`
+    })
 
     gtag('event', '공유선택: 주소복사', {
         'event_category': '공유',
